12c9aa846ca86a081efbf98acc7ef993f4303299
oguzhndlc <oguzdalg@gmail.com>
2025-12-28 03:53:35 +0300

    updte

--- netlify/functions/src/app.js
+++ netlify/functions/src/app.js
@@ -1,10 +1,15 @@
 // netlify/functions/src/app.js
 const express = require("express");
+const router = express.Router();
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
 
-app.use("/users", userRoutes);
+router.use("/users", userRoutes);
+
+app.use("/api", router);
+
+app.use("/.netlify/functions/api", router);
 
 module.exports = app;

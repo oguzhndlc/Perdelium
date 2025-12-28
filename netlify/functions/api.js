483810671ba43a24545ca55f22ef55728d680701
oguzhndlc <oguzdalg@gmail.com>
2025-12-28 03:46:52 +0300

    update

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,25 +1,5 @@
-const express = require("express");
+// netlify/functions/api.js
 const serverless = require("serverless-http");
-const path = require("path");
-const userRoutes = require("./routes/userRoutes");
+const app = require("./src/app");
 
-const app = express();
-app.use(express.json());
-
-// DEBUG: Terminale hangi yolun geldiğini görelim
-app.use((req, res, next) => {
-  console.log(`Gelen İstek: ${req.url}`);
-  next();
-});
-
-// ÇÖZÜM: Express'e hem Netlify'ın tam yolunu hem de kısa yolu tanıtıyoruz
-const router = express.Router();
-router.use("/users", userRoutes);
-
-// 1. Durum: /api/users olarak gelen istekler için
-app.use("/api", router);
-
-// 2. Durum: /.netlify/functions/api/users olarak gelen istekler için
-app.use("/.netlify/functions/api", router);
-
-module.exports.handler = serverless(app);
+exports.handler = serverless(app);
---
+++ netlify/functions/src/app.js
@@ -0,0 +1,10 @@
+// netlify/functions/src/app.js
+const express = require("express");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+module.exports = app;

f50024936643f48c75f7fe2297c954030d13cc64
oguzhndlc <oguzdalg@gmail.com>
2025-12-28 04:59:27 +0300

    profileUpdate

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -2,7 +2,42 @@ const user = JSON.parse(localStorage.getItem("user"));
 
 if (!user) {
   window.location.href = "/pages/login.html";
-} else {
-  document.getElementById("profileName").textContent = user.name;
-  document.getElementById("profileRole").textContent = user.role;
 }
+
+// Görüntüleme alanı
+document.getElementById("profileName").textContent = user.name;
+
+// Formu doldur
+document.getElementById("editName").value = user.name;
+document.getElementById("editUsername").value = user.username;
+
+const editBtn = document.getElementById("editProfileBtn");
+const editCard = document.getElementById("profileEditCard");
+const cancelBtn = document.getElementById("cancelEditBtn");
+
+// Aç
+editBtn.addEventListener("click", () => {
+  editCard.classList.remove("d-none");
+  editBtn.classList.add("d-none");
+});
+
+// Kapat
+cancelBtn.addEventListener("click", () => {
+  editCard.classList.add("d-none");
+  editBtn.classList.remove("d-none");
+});
+
+// Kaydet
+document.getElementById("profileEditForm").addEventListener("submit", e => {
+  e.preventDefault();
+
+  const updatedUser = {
+    ...user,
+    name: document.getElementById("editName").value
+  };
+
+  localStorage.setItem("user", JSON.stringify(updatedUser));
+
+  alert("Profil güncellendi ✅");
+  location.reload();
+});
